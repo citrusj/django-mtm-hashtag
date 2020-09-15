c5295a72303e2682b3718571d87a18bcba8f0717
citrusj <citrusj0123@gmail.com>
2020-09-15 23:20:49 +0900

    hashtag add, delete, hashtag detail

--- board/views.py
+++ board/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from .models import Content, Comment
+from .models import Content, Comment, Tag
 from django.utils import timezone
-from .forms import ContentForm, CommentForm
+from .forms import ContentForm, CommentForm, TagForm
 from django.shortcuts import get_object_or_404
 
 # Create your views here.
@@ -36,9 +36,9 @@ def detail(request, pk):
             return redirect('detail', pk=pk)
     else:
         comment_form = CommentForm()
+        tag_form = TagForm()
 
-    return render(request, 'board/detail.html', {'post': post, 
-    'comments':comments, 'comment_form':comment_form})
+    return render(request, 'board/detail.html', {'post': post, 'comments':comments, 'comment_form':comment_form, 'tag_form':tag_form})
 
 def edit(request, pk):
     post = get_object_or_404(Content, pk=pk)
@@ -63,3 +63,35 @@ def delete_comment(request, pk, comment_pk):
     comment = get_object_or_404(Comment,pk=comment_pk)
     comment.delete()
     return redirect('detail', pk=pk)
+
+def tag_add(request, pk):
+    post = get_object_or_404(Content, pk=pk) #content의 pk를 이용하여 태그가 삽입된 content object를 가져옴
+    tag_form = TagForm(request.POST) #TagForm을 전송받음
+    if tag_form.is_valid(): #Form 유효성 검사. 유효할 경우 아래 코드 수행
+        tag = tag_form.save(commit=False)
+        #TagForm에 따라 Tag objec를 생성하되, 일단 데이터베이스에 저장하지는 않음. (commit=False)
+        tag, created = Tag.objects.get_or_create(name=tag.name)
+        #name이 같은 Tag가 이미 데이터베이스에 있는 경우, object를 생성하지 않고 그냥 해당 object를 변수 tag에 가져옴.
+        #name이 같은 Tag가 없는 경우, create하고 만든 object를 변수 tag에 가져옴.
+        post.tags.add(tag) #post의 tags에 해당 tag를 추가함
+        return redirect('detail',pk=pk) #pk=pk인 detail페이지로 redirect
+
+def tag_home(request):
+    tags = Tag.objects.all()
+    return render(request, 'board/tag.html', {'tags':tags})
+
+def tag_detail(request, pk):
+    tag = get_object_or_404(Tag, pk=pk)
+    tag_posts = tag.content_set.all()
+    return render(request, 'board/tag_detail.html',{'tag':tag, 'tag_posts':tag_posts})
+
+def tag_delete(request, pk , tag_pk):
+    post = get_object_or_404(Content,pk=pk)
+    tag = get_object_or_404(Tag, pk=tag_pk)
+    post.tags.remove(tag)
+
+    if tag.content_set.count() == 0:
+        tag.delete()
+    
+    return redirect('detail',pk=pk)
+    
